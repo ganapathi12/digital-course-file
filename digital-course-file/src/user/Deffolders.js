84a5544e2e4f18c8455e4f4645816b01765ad835
Nidharshan A <cb.en.u4cse18036@cb.students.amrita.edu>
2021-03-24 14:00:59 +0000

    minor changes

--- digital-course-file/src/user/Deffolders.js
+++ digital-course-file/src/user/Deffolders.js
@@ -12,62 +12,74 @@ export default function Deffolders( {currentFolder} ){
             if(currentFolder !== ROOT_FOLDER){
               path.push( { name : currentFolder.name , id : currentFolder.id} )
           }
+          const parents =  [...currentFolder.parents];
+          if(currentFolder!== ROOT_FOLDER){
+              parents.push(currentFolder.id)
+          }
               database.folders.add({
                   name : "Program Outcomes",
                   parentId : currentFolder.id,
                   userId: firebase.auth().currentUser.uid,
                   path : path,
-                  createdAt : database.getTime()
+                  createdAt : database.getTime(),
+                  parents : parents,              
               })
               database.folders.add({
                   name : "Course Outcomes",
                   parentId : currentFolder.id,
                   userId: firebase.auth().currentUser.uid,
                   path : path,
-                  createdAt : database.getTime()
+                  createdAt : database.getTime(),
+                  parents : parents,              
               })
               database.folders.add({
                 name : "Past Papers",
                 parentId : currentFolder.id,
                 userId: firebase.auth().currentUser.uid,
                 path : path,
-                createdAt : database.getTime()
-            })
+                createdAt : database.getTime(),
+                parents : parents,              
+              })
               database.folders.add({
                 name : "Continous Assesment",
                 parentId : currentFolder.id,
                 userId: firebase.auth().currentUser.uid,
                 path : path,
-                createdAt : database.getTime()
+                createdAt : database.getTime(),
+                parents : parents,              
             })
             database.folders.add({
               name : "Attendance",
               parentId : currentFolder.id,
               userId: firebase.auth().currentUser.uid,
               path : path,
-              createdAt : database.getTime()
-          })
+              createdAt : database.getTime(),
+              parents : parents,              
+            })
           database.folders.add({
             name : "P1 materials",
             parentId : currentFolder.id,
             userId: firebase.auth().currentUser.uid,
             path : path,
-            createdAt : database.getTime()
-        })
+            createdAt : database.getTime(),
+            parents : parents,              
+          })
         database.folders.add({
           name : "P2 materials",
           parentId : currentFolder.id,
           userId: firebase.auth().currentUser.uid,
           path : path,
-          createdAt : database.getTime()
-      })
+          createdAt : database.getTime(),
+          parents : parents,              
+        })
       database.folders.add({
         name : "EndSem materials",
         parentId : currentFolder.id,
         userId: firebase.auth().currentUser.uid,
         path : path,
-        createdAt : database.getTime()
-    })
+        createdAt : database.getTime(),
+        parents : parents,              
+      })
     }
   return (
     <>
