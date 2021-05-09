e701331774675c25ba3bfa65c8498e11065f5827
BalajiD <cb.en.u4cse18008@cb.students.amrita.edu>
2021-05-09 19:42:11 +0530

    Changes

--- digital-course-file/src/Routes.js
+++ digital-course-file/src/Routes.js
@@ -1,11 +1,8 @@
 import React, { useState, useEffect, Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-// import PrivateRoutes from "./auth/helper/PrivateRoutes"
-// import Cart from "./core/Cart"
 import Home from './core/Home'
 import Hero from './user/Hero'
 import Signin from './user/Signin'
-// import UserDashboard from "./user/UserDashboard"
 import copyright from './user/copyright'
 import { Link } from 'react-router-dom'
 import { Container, Button, Navbar, Nav } from 'react-bootstrap'
