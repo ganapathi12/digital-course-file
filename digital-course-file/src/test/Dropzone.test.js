97a9a3da5ac7d8b48b5311603ed4c500585e8f43
Gana016 <jayam.ganapathi12@gmail.com>
2021-06-11 08:38:37 +0530

    test

--- digital-course-file/src/test/Dropzone.test.js
+++ digital-course-file/src/test/Dropzone.test.js
@@ -2,6 +2,7 @@ import {React,Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import Dropzone from '../user/Dropzone';
 
+
 it("Unit testing for Drag N Drop", () =>{
     const folder = {id: '1255',name: 'grefd',parentId:'nmhsd'}
     const fragment = document.createElement("Fragment");
